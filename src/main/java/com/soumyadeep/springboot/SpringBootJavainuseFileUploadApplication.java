35f2103f9e8d10de63828c68df0a8c400eaab360
Soumyadeep Saha <SOUMYADEEP_SAHA@CRGL-THIRDPARTY.COM>
2019-01-02 19:39:59 +0530

    first commit

---
+++ src/main/java/com/soumyadeep/springboot/SpringBootJavainuseFileUploadApplication.java
@@ -0,0 +1,14 @@
+package com.soumyadeep.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJavainuseFileUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJavainuseFileUploadApplication.class, args);
+	}
+
+}
+
